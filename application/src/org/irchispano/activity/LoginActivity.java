64abdec2013617f021bbe04e90b3bad33af5d891
Rafael Fernández López <ereslibre@ereslibre.es>
2012-03-16 13:54:06 +0100

    Set host to android.irc-hispano.org

--- application/src/org/irchispano/activity/LoginActivity.java
+++ application/src/org/irchispano/activity/LoginActivity.java
@@ -99,7 +99,7 @@ public class LoginActivity extends Activity implements ServiceConnection {
                     // Server
                     Server s = new Server();
                     s.setAuthentication(a);
-                    s.setHost("irc.irc-hispano.org");
+                    s.setHost("android.irc-hispano.org");
                     s.setIdentity(i);
                     s.setPort(80);
                     s.setCharset("ISO-8859-15");
